2cbf55dfd1e77da5b69ab4ac64685bccbb49ac05
winssps <365423254@qq.com>
2019-07-03 00:56:49 +0800

    增加 登录验证

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const Koa = require('koa');
 const cors = require('koa-cors');
 const bodyParser = require('koa-bodyparser');
-
+const jwt = require('koa-jwt');
 
 const router = require('./routers/index');
 
@@ -24,6 +24,8 @@ app.use(async (ctx, next) => {
   const ms = Date.now() - start;
   ctx.set('X-Response-Time', `${ms}ms`);
 });
+
+app.use(jwt({ secret: 'shared-secret' }));
 
 app
   .use(router.routes())
